401d203930fa1d139496ccbf2624c8663d541e05
Mohammad Rabia Najib <forgitrabia@gmail.com>
2023-01-25 09:53:56 +0300

    initial

---
+++ src/background.jsx
@@ -0,0 +1,24 @@
+import {React,  Component, Fragment } from 'react'
+
+import './custom.css'
+import {Container} from 'react-bootstrap'
+
+
+
+
+
+class Background extends Component {
+     render() {
+          return (
+               <Fragment>
+
+               <Container fluid={true} className="background p-0 "  >
+               
+               </Container>
+
+               </Fragment>
+          )
+     }
+}
+
+export default Background
